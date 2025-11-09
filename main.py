8915dab06bf0e66c9de412024d2836f50f4b5d92
Daksh Pathak <daksh.pathak.ug24@nsut.ac.in>
2025-11-09 20:45:45 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,29 @@
+from art import logo, vs
+from game_data import data
+import random 
+
+def format_data(account):
+    account_name = account['name']
+    account_description = account['description']
+    account_country = account['country']
+    return  f"{account_name}, a {account_description}, from {account_country}."
+
+def check_answer(guess, a_followers, b_followers):
+    if a_followers > b_followers:
+        return guess == 'a'
+    else:
+        return guess == 'b' 
+
+print (logo)
+score = 0  
+game_should_continue = True
+account_a = random.choice(data)
+account_b = random.choice(data)
+if account_a == account_b: 
+    account_b = random.choice(data) 
+
+print(f"Compare A: {format_data(account_a)}")
+print(vs)
+print(f"Against B: {format_data(account_b)}")
+
+guess = input("Who has more followers? Type 'A' or 'B': ").lower()
